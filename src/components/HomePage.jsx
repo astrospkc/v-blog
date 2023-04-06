c8dac3d699f96135d5dcf7132ba705f539a13db4
astrospkc <punamkumari399@gmail.com>
2023-04-06 19:55:29 +0530

    fixed read note

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -113,16 +113,16 @@ const HomePage = () => {
       {/* show modal only --read ony */}
       {showOnlyModal && (
         <div
-          className="fixed w-full bg-zinc-700 h-screen flex flex-col"
+          className="fixed w-full bg-zinc-700 h-screen flex flex-col "
           ref={ref}
         >
-          <div className=" flex flex-col gap-5  flex-grow mx-16">
+          <div className=" flex flex-col gap-5 bg-zinc-800 flex-grow mx-16 my-16">
             <h1
               id="title"
               name="title"
               onChange={handleChange}
               value={note.title}
-              className="text-center text-white"
+              className="text-center text-white text-3xl p-2 font-mono"
             >
               {note.title}
             </h1>
@@ -130,20 +130,19 @@ const HomePage = () => {
               name="description"
               id="description"
               onChange={handleChange}
-              className="bg-zinc-800 text-white p-3 rounded-md  outline-none flex-grow focus:shadow-xl "
+              className=" text-white p-3 rounded-md font-mono outline-none flex-grow focus:shadow-xl "
             >
               {note.description}
             </p>
-
-            <div className="flex flex-row mb-5">
-              <button
-                onClick={closeOnlyModal}
-                ref={refClose}
-                className="flex w-20 text-sm transform justify-center rounded-2xl border-b-4 border-gray-500 bg-gray-400  px-3 py-1 text-gray-100 transition  duration-200 ease-in-out hover:translate-y-px hover:border-b-2 "
-              >
-                close
-              </button>
-            </div>
+          </div>
+          <div className="flex flex-row mb-5">
+            <button
+              onClick={closeOnlyModal}
+              ref={refClose}
+              className="flex w-20 text-sm transform justify-center rounded-2xl border-b-4 border-gray-500 bg-gray-400  px-3 py-1 text-gray-100 transition  duration-200 ease-in-out hover:translate-y-px hover:border-b-2 "
+            >
+              close
+            </button>
           </div>
         </div>
       )}
